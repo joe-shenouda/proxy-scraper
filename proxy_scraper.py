0b22f8cc9c3c55535ad1af9f5edff64d1ab906e5
Joe Shenouda <info@shenouda.nl>
2023-03-18 11:31:42 +0100

    Update proxy_scraper.py

--- proxy_scraper.py
+++ proxy_scraper.py
@@ -1,27 +1,41 @@
-import requests
-from bs4 import BeautifulSoup
+import asyncio
+import re
+import httpx
 
-# Send a request to the URL and get the response
-url = "https://free-proxy-list.net/"
-response = requests.get(url)
+async def fetch(url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+    return response.text
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+async def extract_proxies(url):
+    content = await fetch(url)
+    pattern = re.compile(r"\d{1,3}(?:\.\d{1,3}){3}(?::\d{1,5})?")
+    proxies = re.findall(pattern, content)
+    return proxies
 
-# Find the table containing the proxy information
-proxy_table = soup.find("table", attrs={"class": "table-bordered"})
+async def main():
+    proxy_sources = [
+        "https://spys.me/proxy.txt",
+        "https://api.proxyscrape.com/?request=getproxies&proxytype=http",
+        "https://api.proxyscrape.com/?request=getproxies&proxytype=socks4",
+        "https://api.proxyscrape.com/?request=getproxies&proxytype=socks5",
+    ]
 
-# If table is found, iterate over its rows and get proxy information
-if proxy_table is not None:
-    with open("proxies.txt", "w") as file:
-        for row in proxy_table.find_all("tr")[1:]:
-            columns = row.find_all("td")
-            ip = columns[0].get_text()
-            port = columns[1].get_text()
-            country_code = columns[2].get_text()
-            anonymity = columns[4].get_text()
-            https = columns[6].get_text()
-            file.write(f"{ip}:{port}\n")
-    print("Proxies written to proxies.txt")
-else:
-    print("Could not find proxy table on the page.")
+    proxies = []
+    tasks = []
+
+    for source in proxy_sources:
+        tasks.append(asyncio.ensure_future(extract_proxies(source)))
+
+    results = await asyncio.gather(*tasks)
+
+    for result in results:
+        proxies.extend(result)
+
+    with open("proxies.txt", "w") as f:
+        f.write("\n".join(proxies))
+
+    print("Proxies have been saved to proxies.txt")
+
+if __name__ == "__main__":
+    asyncio.run(main())
